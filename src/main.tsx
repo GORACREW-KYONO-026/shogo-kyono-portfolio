f6f7ad216a62a94aef119eb62420a80af41b0f66
GORACREW-KYONO-026 <s.kyono@goracrew.co.jp>
2025-05-20 20:39:56 +0900

    add: add package biome formmater

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+  // NOTE:NOT FOUNDページへリダイレクトさせる
+  // notfound.htmlは public配下に置いてエラーページを表示させる
+  // window.location.href = '/notfound.html';
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
